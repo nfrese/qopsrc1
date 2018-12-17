81e54c20bf6c5ecec4f360ad6d74379e560c574c
Norbert Frese <nf2.email@gmail.com>
2018-12-17 16:01:49 +0100

    add license

--- qopwebui/src/main/java/at/qop/ws/QOPBatchCalculationServlet.java
+++ qopwebui/src/main/java/at/qop/ws/QOPBatchCalculationServlet.java
@@ -1,11 +1,29 @@
+/* 
+ * Copyright (C) 2018 Norbert Frese
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+*/
+
 package at.qop.ws;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.TreeSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
