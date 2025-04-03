07e4bf416a7e62db44de80646c94cdf1431a4dce
norbert frese <norbert.frese@prisms-solutions.com>
2025-04-03 09:57:51 +0200

    imports

--- qoplib/src/test/java/at/qop/qoplib/osmosis/OsmosisPoisToDb.java
+++ qoplib/src/test/java/at/qop/qoplib/osmosis/OsmosisPoisToDb.java
@@ -7,12 +7,10 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
